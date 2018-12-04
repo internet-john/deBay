6edfdf615f17b199b10a8974482923ca8d91cbe0
etzJohn <holman.jx@gmail.com>
2018-12-03 19:09:36 -0800

    add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
 import Contract from './utils/contractInstantiator';
 
-console.log('hey');
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello world!</h1>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.app'));
