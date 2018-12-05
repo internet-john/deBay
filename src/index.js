ac978af2aba494704e323fef9a7eff49edda9fee
etzJohn <holman.jx@gmail.com>
2018-12-04 20:42:05 -0800

    wire up web3 and contract interaction

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom'
 import Contract from './js/utils/contractInstantiator.js';
 import ListingPage from './js/components/ListingPage.jsx';
 
-ReactDOM.render(<ListingPage />, document.querySelector('.app'));
+ReactDOM.render(<ListingPage contract={Contract}/>, document.querySelector('.app'));
